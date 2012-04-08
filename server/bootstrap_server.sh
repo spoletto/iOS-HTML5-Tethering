d3dd3d61aba6143fd9ad355ec92a233b42b677ba
Stephen Poletto <stephenpoletto@gmail.com>
2012-04-08 14:43:28 -0400

    Adding server bootstrap script to install needed tools on the EC2 box.

---
+++ server/bootstrap_server.sh
@@ -0,0 +1,25 @@
+# Install the essentials.
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y rcconf
+sudo apt-get install -y build-essential
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y git-core
+
+# Install Node.
+wget http://nodejs.org/dist/node-v0.6.13.tar.gz
+tar xzf node-v0.6.13.tar.gz
+cd node-v0.6.13
+sudo ./configure --prefix=/usr
+make
+sudo make install
+
+# Install npm.
+cd ~
+git clone http://github.com/isaacs/npm.git
+cd npm
+sudo make install
+
+# Install the node modules we'll need.
+cd ~
+npm install websocket-server
