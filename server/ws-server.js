597ba3c4c12bb5c143ea464003d2e9dba087e337
Stephen Poletto <stephenpoletto@gmail.com>
2012-04-08 15:33:52 -0400

    Adding server-side websocket server.

---
+++ server/ws-server.js
@@ -0,0 +1,18 @@
+/* 
+ * Server-side websocket server.
+ *
+ */
+
+var ws = require("websocket-server");
+
+var server = ws.createServer();
+
+// Add a listener for new websocket connections.
+server.addListener("connection", function(connection) {
+	console.log("Websocket connection established.")
+	connection.addListener("message", function(msg) {
+		console.log("recevied message " + msg);
+	});
+});
+
+server.listen(80);
