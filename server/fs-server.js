23fc25dcc908b362eb2c274f5f4fdb9e94d5e6d4
Stephen Poletto <stephenpoletto@gmail.com>
2012-04-24 06:23:58 -0400

    Fixing whitespace.

--- server/fs-server.js
+++ server/fs-server.js
@@ -7,18 +7,18 @@ var express = require("express");
 var app = express.createServer();
 
 app.get("/", function(req, res) {
-	res.redirect("/index.html");
+    res.redirect("/index.html");
 });
 
 app.configure(function() {
-	app.use(express.methodOverride());
-	app.use(express.bodyParser());
-	app.use(express.static(__dirname + '/public'));
-	app.use(express.errorHandler({
-		dumpExceptions: true,
-		showStack: true
-	}));
-	app.use(app.router);
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+    app.use(app.router);
 });
 
 app.listen(80);
--- server/ws-server.py
+++ server/ws-server.py
@@ -15,63 +15,63 @@ outbound_packets = defaultdict(int)
 connection = None
 
 class PacketSniffer(threading.Thread):
-	def __init__(self):
-		threading.Thread.__init__(self) 
+    def __init__(self):
+        threading.Thread.__init__(self) 
 
-	def run(self):
-		global connection
-		while (True):
-			pkt = sniff(iface="eth0", count=1)
-			if not pkt[0].haslayer(IP) or not pkt[0].haslayer(TCP):
-				continue # Just handle TCP for now.
-			pkt = pkt[0][IP]
-			
-			if outbound_packets[(pkt.src, pkt.sport)] > 0:
-				outbound_packets[(pkt.src, pkt.sport)] -= 1
-				
-				# Modify the destination address back to the address of the TUN on the host.		
-				pkt.dst = "10.0.0.1"
-				del pkt[TCP].chksum
-				del pkt[IP].chksum
-				pkt.show2() # Force recompute the checksum
-	
-				if connection:
-					connection.write_message(str(pkt).encode('base64'))
-			
+    def run(self):
+        global connection
+        while (True):
+            pkt = sniff(iface="eth0", count=1)
+            if not pkt[0].haslayer(IP) or not pkt[0].haslayer(TCP):
+                continue # Just handle TCP for now.
+            pkt = pkt[0][IP]
+            
+            if outbound_packets[(pkt.src, pkt.sport)] > 0:
+                outbound_packets[(pkt.src, pkt.sport)] -= 1
+                
+                # Modify the destination address back to the address of the TUN on the host.        
+                pkt.dst = "10.0.0.1"
+                del pkt[TCP].chksum
+                del pkt[IP].chksum
+                pkt.show2() # Force recompute the checksum
+    
+                if connection:
+                    connection.write_message(str(pkt).encode('base64'))
+            
 
 class Handler(WebSocketHandler):
-	def open(self):
-		global connection
-		print "New connection opened."
-		connection = self
+    def open(self):
+        global connection
+        print "New connection opened."
+        connection = self
 
-	def manipulate_outgoing_packet(self, message):
-		# Add the original packet to the NAT table.
-		global outbound_packets
-		ipPacket = IP(message)
-		outbound_packets[(ipPacket.dst, ipPacket.dport)] += 1
-		
-		# Modify the source IP address and recalculate the checksum.
-		ipPacket.src = "10.202.43.31"
-		del ipPacket[TCP].chksum
-		del ipPacket[IP].chksum
-		send(ipPacket)
-		
-	def on_message(self, message):
-		self.manipulate_outgoing_packet(message.decode('base64'))
+    def manipulate_outgoing_packet(self, message):
+        # Add the original packet to the NAT table.
+        global outbound_packets
+        ipPacket = IP(message)
+        outbound_packets[(ipPacket.dst, ipPacket.dport)] += 1
+        
+        # Modify the source IP address and recalculate the checksum.
+        ipPacket.src = "10.202.43.31"
+        del ipPacket[TCP].chksum
+        del ipPacket[IP].chksum
+        send(ipPacket)
+        
+    def on_message(self, message):
+        self.manipulate_outgoing_packet(message.decode('base64'))
 
-	def on_close(self):
-		global connection
-		print "Connection closed."
-		connection = None
+    def on_close(self):
+        global connection
+        print "Connection closed."
+        connection = None
 
-	def allow_draft76(self):
-		return True
+    def allow_draft76(self):
+        return True
 
 sniffingThread = PacketSniffer()
 sniffingThread.daemon = True
 sniffingThread.start()
-			
+            
 wsServer = HTTPServer(Application([("/websocket/", Handler)]))
 print "Server started."
 wsServer.listen(8080)
