f108958f0d8e38da3bba806d0dfcecfea4bbdbd7
Stephen Poletto <stephenpoletto@gmail.com>
2012-04-24 03:11:50 -0400

    Cleaning up client side code.

--- client/client-ws-server.py
+++ client/client-ws-server.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python
 
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado.web import Application
 from tornado.websocket import WebSocketHandler
-import threading
+from tornado.httpserver import HTTPServer
+from tornado.web import Application
+from tornado.ioloop import IOLoop
+
 import subprocess
+import threading
 import os
 
+# Initialize the tun0 device.
 tun = os.open('/dev/tun0', os.O_RDWR)
 
+# Assign a fixed, known alias address to the wifi card.
+subprocess.check_call('sudo /sbin/ifconfig en1 inet 169.254.134.89 netmask 255.255.0.0 alias', shell=True)
+
+# Bring up the tun device and assign the IP address 10.0.0.1 to it.
 subprocess.check_call('sudo ifconfig tun0 10.0.0.1 10.0.0.1 netmask 255.255.255.0 up', shell=True)
+
+# Modify the IP routing table on the host machine to funnel all network traffic through tun0.
 subprocess.check_call('sudo route delete default', shell=True)
 subprocess.check_call('sudo route add default 10.0.0.1', shell=True)
 
+# Currently only capable of managing a single websocket connection.
+# Not thread-safe.
 connection = None
 
 class TunReader(threading.Thread):
-    def __init__(self, server):
-        self.server = server
+    def __init__(self):
         threading.Thread.__init__(self) 
 
     def run(self):
         global connection
         global tun
-        print "Tunnel listening thread started."
         while (True):
-            print "In while loop"
-            dataFromTun = os.read(tun, 1500)
-            dataFromTun = dataFromTun.encode('base64')       
-            print "message received from tunnel: " + dataFromTun
-            print "connection is in other thread " + str(connection)
+            dataFromTun = os.read(tun, 1500).encode('base64')  
             if connection:
-                print "sending message to websocket"
                 connection.write_message(dataFromTun)
 
 class Handler(WebSocketHandler):
@@ -40,7 +43,6 @@ class Handler(WebSocketHandler):
             global connection
             print "New connection opened."
             connection = self
-            print "connection is " + str(connection)
 
         def on_message(self, message):
             global tun
@@ -57,11 +59,11 @@ class Handler(WebSocketHandler):
         def allow_draft76(self):
             return True
 
-
-wsServer = HTTPServer(Application([("/websocket", Handler)]))
-tunThread = TunReader(None)
+tunThread = TunReader()
 tunThread.daemon = True
 tunThread.start()
+
+wsServer = HTTPServer(Application([("/websocket", Handler)]))
 print "Server started."
 wsServer.listen(6354, "169.254.134.89")
 IOLoop.instance().start()
